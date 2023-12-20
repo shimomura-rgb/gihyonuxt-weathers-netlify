396557b39d7295ecda771a5c43d2a8f213304d25
Shimomura-rgb <0af325221xc5x6t@gmail.com>
2023-12-20 18:05:39 +0900

    nuxt.config.ts 修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      weatherInfoUrl: "https://api.openweathermap.org/data/2.5/weather",
+      weathermapAppid: ""
+    }
+  }
 })
